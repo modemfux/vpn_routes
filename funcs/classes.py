b3819ac4cc654b4dbf2e58ab4a8254185fe5e7ec
modemfux <modemfux@gmail.com>
2023-01-28 11:45:38 +0300

    Added new method to generate "ip route" config for FRR and return it as list. Also it update two json files: backup and primary. Backup takes old values, primary - new

--- funcs/classes.py
+++ funcs/classes.py
@@ -1,4 +1,4 @@
-import re
+import json
 import time
 import paramiko
 from nix_functions import get_ip_addresses_from_url
@@ -21,14 +21,24 @@ class OwnSSH:
         self.ssh = client.invoke_shell()
         print(f'Connection to {self.ip} established.')
 
+    def close(self):
+        self.ssh.close()
+        print(f'SSH connection to {self.ip} is closed.')
+
+    # Global swithes for current state
+
     is_root = False
     is_vtysh = False
+
+    # Context manager's function
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.ssh.close()
+
+    # Methods for interacting with Linux
 
     def upgrade_to_root(self):
         self.ssh.send('\n')
@@ -46,6 +56,16 @@ class OwnSSH:
         if 'root' in prompt:
             self.is_root = True
 
+    def exit_from_root(self):
+        self.ssh.send('\n')
+        time.sleep(0.5)
+        output = self.ssh.recv(5000).decode('utf-8')
+        if 'root@' in output:
+            self.ssh.send('exit\n')
+            time.sleep(0.5)
+            self.ssh.recv(5000)
+            self.is_root = False
+
     def enter_to_vtysh(self):
         self.ssh.send('vtysh\n')
         time.sleep(0.5)
@@ -54,6 +74,31 @@ class OwnSSH:
         time.sleep(0.5)
         self.ssh.recv(5000)
         self.is_vtysh = True
+
+    def get_routes_from_urls(
+        self,
+        urls,
+        nexthop,
+        server='8.8.8.8',
+        filename='vpn_routes.json'
+        ):
+        if not isinstance(urls, list):
+            urls = [urls]
+        ip_list, url_dict = get_ip_addresses_from_url(
+            urls,
+            server,
+            full_result=True
+        )
+        backup = 'previous_' + filename
+        with open(filename) as src, open(backup, 'w') as dst:
+            json_urls = json.load(src)
+            dst.write(json.dumps(json_urls))
+        with open(filename, 'w') as dst:
+            dst.write(json.dumps(url_dict))
+        routes = [f'ip route {ip} 255.255.255.255 {nexthop} tag 65001' for ip in ip_list]
+        return routes
+
+    # Methods for interacting with FRR
 
     def exit_from_vtysh(self):
         if self.is_vtysh:
@@ -65,10 +110,6 @@ class OwnSSH:
             self.is_vtysh = False
         else:
             print('You are not in FRR vtysh')
-
-    def close(self):
-        self.ssh.close()
-        print(f'SSH connection to {self.ip} is closed.')
 
     def send_config_commands(self, commands, silent=False):
         if not isinstance(commands, list):
